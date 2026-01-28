982ac99cb9d2cea6bf91274d34f5501e5da8ab40
lauramarcelli <laura.marcelli@gmail.com>
2026-01-28 11:59:03 -0300

    dd README with project description

--- game.py
+++ game.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 import random
 
-words = ["python", "java", "php", "javascript", "ruby"]
+words = ["sol", "luna", "estrella", "cometa", "planeta", "agua", "aire", "tierra", "fuego", "nieve", "arbol", "flor", "fruto"
+"arbusto", "hoja", "piedras", "montañas", "rios", "mar"]
 word = random.choice(words)
 wrong_letters = []
 guess_word = ["_" for _ in word]
@@ -12,70 +13,78 @@ def play():
     
     if lives > 0 and "_" in guess_word:
 
-        label_word.config(text=f"Palabra: {' '.join(guess_word)}")
-        label_lives.config(text=f"Vidas restantes: {lives}")
-        label_wrong.config(text=f"Letras incorectas: {', '.join(wrong_letters)}")
+        
         guess = entry_guess.get().lower()
         entry_guess.delete(0, tk.END)
 
         if len(guess) !=1 or not guess.isalpha():
             label_wrong.config(text="Error: Ingrese una sola letra ")
+            return
 
-        if guess in guess_word:
+        if guess in guess_word or guess in wrong_letters:
             label_wrong.config(text="Ya ingresaste esa letra")
-
+            return
+        
         if guess in word:
-            label_wrong.config(text="Bien hecho!")
             for l in range(len(word)):
                 if word[l] == guess:
                     guess_word[l] = guess
+            label_wrong.config(text="Bien hecho!")
         else:
-            label_wrong.config(text="Letra incorrecta")
             lives -= 1
             wrong_letters.append(guess)
+            label_wrong.config(text="Letra incorrecta")
+
+        label_word.config(text=f"Palabra: {' '.join(guess_word)}")
+        label_lives.config(text=f"Vidas restantes: {lives}")
+        label_wrong.config(text=f"Letras incorectas: {', '.join(wrong_letters)}"
+        if wrong_letters else "Incorrectas: ")
 
         if "_" not in guess_word:
-            label_wrong.config(text="Felicidades, adivinaste la palabra")
+            label_wrong.config(text="🥳Felicidades, adivinaste la palabra🎊")
 
         if lives == 0:
-            label_wrong.config(text="Lo siento, perdiste")
+            label_wrong.config(text="🥲Lo siento, perdiste")
             label_word.config(text=f"La palabra era: {word}")
          
 
 #ventana
 root = tk.Tk()
-root.title("Adivina la palabra")
-root.geometry("400x300")
 
+root.title("Adivina la palabra - Versión: Naturaleza")
+root.geometry("500x400")
+
+main_frame = tk.Frame(root)
+main_frame.pack(expand=True)
 
 #labels
-label_word = tk.Label(root, text="Palabra:____")
+label_word = tk.Label(main_frame, text="Palabra: _ _ _ _ _")
 label_word.pack()
 label_word.config(font=("Arial", 16))
 label_word.config(fg="blue")
 label_word.pack(padx=10, pady=10)
 
-label_lives = tk.Label(root, text="Vidas: 6")
+label_lives = tk.Label(main_frame, text="Vidas: 6")
 label_lives.pack()
 label_lives.config(font=("Arial", 16))
 label_lives.config(fg="blue")
 label_lives.pack(padx=10, pady=10)
 
-label_wrong = tk.Label(root, text="Incorrectas: ")
+label_wrong = tk.Label(main_frame, text="Incorrectas: ")
 label_wrong.pack()
 label_wrong.config(font=("Arial", 16))
 label_wrong.config(fg="blue")
 label_wrong.pack(padx=10, pady=10)
 
 #entrada
-entry_guess = tk.Entry(root)
+entry_guess = tk.Entry(main_frame)
 entry_guess.pack()
 entry_guess.config(font=("Arial", 16))
 entry_guess.config(fg="blue")
 entry_guess.pack(padx=10, pady=10)
 
 #boton
-button_play = tk.Button(root, text="jugar", command=play)
+button_play = tk.Button(main_frame, text="jugar", command=play)
 button_play.pack()
 button_play.config(font=("Arial", 16))
 button_play.config(fg="blue")
---
+++ readme.md
@@ -0,0 +1,66 @@
+# 🎮 Adivina la Palabra -Juego en Python con Tkinter
+
+Este proyecto consiste en el desarrollo de un **juego interactivo de adivinanza de palabras**, similar al juego del ahorcado, implementado en **Python** utilizando una **interfaz gráfica con Tkinter**.
+
+---
+
+## 🧠 Descripción del juego
+
+- Al iniciar el juego, el programa selecciona una palabra al azar.
+- La palabra se muestra oculta utilizando guiones bajos (`_`).
+- El jugador ingresa letras de a una por vez.
+- Si la letra es incorrecta, se pierde una vida.
+- El juego finaliza cuando el jugador adivina la palabra o se queda sin vidas.
+
+---
+
+## 💻 Interfaz gráfica
+
+La interfaz gráfica fue desarrollada utilizando **Tkinter** e incluye:
+
+- Visualización de la palabra oculta
+- Contador de vidas restantes
+- Lista de letras incorrectas
+- Campo de texto para ingresar letras
+- Botón para jugar cada turno
+
+La interacción se realiza mediante eventos, sin uso de bucles infinitos
+
+---
+
+## 🛠️ Tecnologías utilizadas
+
+- Python
+- Tkinter
+- Modulo `random`
+
+---
+
+## ▶️ Cómo ejecutar el proyecto
+
+### Requisitos
+
+Tener instalado:
+
+- Python
+- Tkinter
+
+### Ejecutar en consola
+
+python3 main.py
+
+
+## 🎯 Objetivos del proyecto
+
+- Practica lógica de programación en Python
+- Aprender a trabajar con interfaces gráficas
+- Comprender el modelo de programación orientada a eventos
+- Separar estado del juego y presentación visual
+
+## 🌟 Agradecimientos y Autoria
+
+- Agradecimientos: a la Ing. Alicia San Román y ADA por brindarme el espacio y tiempo para este aprendizaje.  
+- Proyecto desarrollado por Laura Marcelli en el marco de la cursada de Introducción a Python dictado por ADA.
+
+
+
